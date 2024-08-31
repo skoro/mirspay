e73000cd7885ffd2c9e4be47a5ae320bc376fdce
Oleksii Skorobogatko <skorobogatko.oleksii@gmail.com>
2024-08-31 13:14:27 +0300

    #4 remove unused statuses, format statuses to a string

--- src/Entity/OrderStatus.php
+++ src/Entity/OrderStatus.php
@@ -7,9 +7,12 @@ namespace App\Entity;
 enum OrderStatus: string
 {
     case CREATED = 'created';
-    case ERROR = 'error';
-    case FINISHED = 'finished';
     case PAYMENT_PENDING = 'payment_pending';
     case PAYMENT_RECEIVED = 'payment_received';
     case PAYMENT_FAILED = 'payment_failed';
+
+    public static function formattedString(string $delimiter = ', '): string
+    {
+        return implode($delimiter, array_map(fn ($enum) => $enum->value, self::cases()));
+    }
 }
