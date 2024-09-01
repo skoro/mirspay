cb2e4c94bc24bb7a922931091123a486e6393ef1
Oleksii Skorobogatko <skorobogatko.oleksii@gmail.com>
2024-09-01 18:00:35 +0300

    #4 renamed

---
+++ src/Subscriber/Exception/NotificationChannelNotRegisteredException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Subscriber\Exception;
+
+use Exception;
+
+final class NotificationChannelNotRegisteredException extends Exception
+{
+    public function __construct(
+        public readonly string $notificationChannelType,
+    ) {
+        parent::__construct("Subscriber notification channel \"$this->notificationChannelType\" is not registered.");
+    }
+}
