b29120dfafe10ecc398b8171612b2a4201d8bee1
Oleksii Skorobogatko <skorobogatko.oleksii@gmail.com>
2024-09-01 19:18:30 +0300

    #4 subscriber notification sending handler

--- src/EventListener/NotifySubscribersListener.php
+++ src/EventListener/NotifySubscribersListener.php
@@ -5,19 +5,31 @@ declare(strict_types=1);
 namespace App\EventListener;
 
 use App\Event\OrderStatusWasChanged;
+use App\Message\NotifySubscriber;
 use App\Repository\SubscriberRepository;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 final class NotifySubscribersListener
 {
     public function __construct(
         private readonly SubscriberRepository $subscriberRepository,
+        private readonly MessageBusInterface $messageBus,
     ) {
     }
 
     #[AsEventListener(event: OrderStatusWasChanged::class)]
     public function onOrderStatusWasChanged(OrderStatusWasChanged $event): void
     {
+        // TODO: batch/chunks or expecting subscriber list quite small ?
         $subscribers = $this->subscriberRepository->getList($event->order->getStatus());
+
+        foreach ($subscribers as $subscriber) {
+            $this->messageBus->dispatch(new NotifySubscriber(
+                orderId: $event->order->getId(),
+                subscriberId: $subscriber->getId(),
+                response: $event->response,
+            ));
+        }
     }
 }
---
+++ src/MessageHandler/NotifySubscriberHandler.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\MessageHandler;
+
+use App\Message\NotifySubscriber;
+use App\Repository\OrderRepository;
+use App\Repository\SubscriberRepository;
+use App\Subscriber\Action\SendSubscriberNotificationAction;
+use App\Subscriber\Exception\ChannelMessageNotRegistered;
+use App\Subscriber\Exception\NotificationChannelNotRegisteredException;
+use Doctrine\ORM\EntityNotFoundException;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+#[AsMessageHandler]
+final readonly class NotifySubscriberHandler
+{
+    public function __construct(
+        private SendSubscriberNotificationAction $sendSubscriberNotificationAction,
+        private OrderRepository                  $orderRepository,
+        private SubscriberRepository             $subscriberRepository,
+        private LoggerInterface                  $logger,
+    ) {
+    }
+
+    public function __invoke(NotifySubscriber $message): void
+    {
+        try {
+            $order = $this->orderRepository->find($message->orderId);
+            if (! $order) {
+                throw new EntityNotFoundException("Order \"{$message->orderId}\" not found.");
+            }
+
+            $subscriber = $this->subscriberRepository->find($message->subscriberId);
+            if (! $subscriber) {
+                throw new EntityNotFoundException("Subscriber \"{$message->subscriberId}\" not found.");
+            }
+
+            $this->sendSubscriberNotificationAction->sendNotification($order, $subscriber, $message->response);
+        } catch (EntityNotFoundException|ChannelMessageNotRegistered|NotificationChannelNotRegisteredException $e) {
+            $this->logger->error($e->getMessage());
+            return;
+        }
+    }
+}
